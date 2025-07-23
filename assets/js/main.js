9a6f401c6095cb34a6aa1e404d5d01f47456c637
Nurudeen Abdulsalaam <olaitansalaam2012@gmail.com>
2025-07-23 19:28:27 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,7 +126,7 @@
   const typed = select('.typed')
   if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
+    typed_strings = typed_strings.split(',').map(str => str.trim())
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
